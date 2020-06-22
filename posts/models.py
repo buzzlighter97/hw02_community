7b12d1d974431f2c87cadb5c6ded12c11537e483
Ivan Ivanov <buzzlighter97@gmail.com>
2020-06-22 18:22:43 +0300

    The third commit

--- posts/models.py
+++ posts/models.py
@@ -8,9 +8,6 @@ class Group(models.Model):
     slug = models.SlugField(unique=True)
     description = models.TextField()
 
-    class Meta():
-        ordering = ['-pub_date']
-
     def __str__(self):
         return self.title
 
@@ -19,4 +16,7 @@ class Post(models.Model):
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     group = models.ForeignKey(Group, on_delete=models.SET_NULL, 
-                                related_name = 'group', blank=True, null=True)
+                                related_name = 'groups_posts', blank=True, null=True)
+    
+    class Meta:
+        ordering = ['-pub_date']
